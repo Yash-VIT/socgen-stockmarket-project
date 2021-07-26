dbe7352929f826eaefbad5803df12bee48be585a
Yash-VIT <yashrajgithub@gmail.com>
2021-07-26 09:20:02 +0530

    Added Home Page and Navigation

---
+++ frontend/stock-app/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { CompanyListComponent } from './components/company-list/company-list.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CompanyListComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ frontend/stock-app/src/app/models/company/company-dto.ts
@@ -0,0 +1,10 @@
+export class CompanyDto {
+    id: String;
+    companyNamye :String;
+    turnover: number;
+	ceo: String;
+	exchangeListing: boolean;
+	sector: String;
+	writeup : String;
+	companyCode : String;
+}
